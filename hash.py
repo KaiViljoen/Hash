22e5a3d9f91fb8c6dbbc00463ab067097f7defc9
Tenjin979 <86575616+Tenjin979@users.noreply.github.com>
2021-06-29 01:32:41 +0200

    Update hash.py

--- hash.py
+++ hash.py
@@ -3,7 +3,7 @@
 
 from hashlib import __all__
 from random import randrange, choice
-from sys import stdout
+from sys import stdout, argv
 from time import sleep
 
 # type output to screen
@@ -11,16 +11,27 @@ def type(seq):
   for char in seq:
     stdout.write(char)
     stdout.flush()
-    time.sleep(0.05)
+    time.sleep(0.005)
     
     
 # Pseudo-random generate hash
-def random():
-  ''' Import correct hashes and make them usable and importable, 
-      import only specefied hash upon usage and encypt and type
-      pseudo-random number recursively '''
-  
-  # Append all usefull hashtypes to local import list    
-  a=[l for l in a if l.startswith('s') or if l.startswith('m')];a =[l for l in a if 'k' not in l]; a.append(__all__)
-  exec('from hashlib import %s'%(random(a)))
-  globals()[random(a)](randrange(1,10**100).hexdigest()
+def random(hashtype=None, array=None):  
+   # Append all usefull hashtypes to local import list    
+   a=[l for l in a if l.startswith('s') or if l.startswith('m')];a =[l for l in a if 'k' not in l]; a.append(__all__)
+   try:
+       if hashtype ==None:
+         x=choice(a)
+         exec('from hashlib import %s'%(x))
+       elif hashtype !=None:
+          x=hashtype
+          exec('from hashlib import %s'%(x))
+       else: pass
+    except Error as e:
+          type('Unsupported Hash-Type ...')
+    v=globals()[x](f'%i'.encode('utf-8')%(randrange(1,10**100))).hexdigest() # generate call
+    if array !=None: array.append(v)
+    else: print(x)
+
+hash =argv[1]
+if hash =='random': random()
+elif hash!='random': pass
