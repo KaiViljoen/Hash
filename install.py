deedbfdd5b634255599b49fa983a4a82f62aa488
Tenjin979 <86575616+Tenjin979@users.noreply.github.com>
2021-07-02 11:12:16 +0200

    Update install.py

--- install.py
+++ install.py
@@ -2,6 +2,7 @@ from os import path,system
 from sys import stdout
 
 system('mv -r %s/hash /usr/bin/'%(path.abspath('')))
+system('chmod +x /usr/bin/hash/*')
 a =input('[-] Which Shell do you use [zsh, bash, etc]?: ')
-f=open('/bin/%s'%(a),a);f.write('export PATH=$PATH:/usr/bin/hash')
+f=open('/bin/%s'%(a));f.write('export PATH=$PATH:/usr/bin/hash');f.close()
 print('\n[+] Installation Successful, run "hsh" to access RandHash ...")
