024e32dafa0a2be4d50544af529cc1f5a7832854
Tenjin979 <86575616+Tenjin979@users.noreply.github.com>
2021-07-03 00:00:22 +0200

    Update install.py

--- install.py
+++ install.py
@@ -1,8 +1,22 @@
-from os import path,system
+#Written by @Tenjin979 and posted to Github.
+#!/usr/bin/python3
+
+from os import path,system,getcwd
 from sys import stdout
 
-system('mv -r %s/hash /usr/bin/'%(path.abspath('')))
-system('chmod +x /usr/bin/hash/*')
-a =input('[-] Which Shell do you use [zsh, bash, etc]?: ')
-f=open('/bin/%s'%(a));f.write('export PATH=$PATH:/usr/bin/hash');f.close()
+z=['zsh','bash','sh']
+
+#Move files and assign execute permissions.
+system('mv %s/hash/hash/ /usr/bin/'%(getcwd()))
+system('chmod +x /usr/bin/')
+
+#add files to PATH permanently.
+for i in z: 
+    if path.exists('%s/%s'%(getcwd(),i)): 
+       f=open('%s/.%rc'%(getcwd(),i)
+       f.write('export PATH=$PATH:/usr/bin/hash/')
+       f.close()
+    else: 
+         print('[-] Installation Unsuccessful, please report this to the Developer on Github @Tenjin979 ...')
+         exit()
 print('\n[+] Installation Successful, run "hsh" to access RandHash ...")
