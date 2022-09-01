be22dcc5b4706e8bed7e3e09b829ef3dca7d3486
KaiViljoen <Kai.Viljoen@protonmail.com>
2022-09-01 15:52:31 +0200

    Major Upgrades on this commit

--- install.py
+++ install.py
@@ -1,23 +1,65 @@
-#Written by @Tenjin979 and posted to Github.
+#Author @Github.com/KaiViljoen/
 #!/usr/bin/python3
 
 from os import path,system,getcwd,chdir,listdir
 from sys import stdout
+from time import sleep
 from getpass import getuser as user
 
-#Move files and assign rwx permissions.
-system('sudo rm -rfv /usr/bin/hash && sudo mv -f /home/$USER/Hash/hash/* /usr/bin/ && sudo chown -R $USER /usr/bin/hsh /usr/bin/hash.py && sudo chmod g+rwx,u+rwx /usr/bin/hsh /usr/bin/hash.py')
-
-#add files to PATH permanently.
 chdir('/home/%s'%(user()))
+global x
 x=list(listdir())
 x=[a for a in x if a.endswith('rc')]
-if path.exists(x[0]):
-   system('echo export PATH=/usr/bin/hash.py >> ~/.{sh}rc && echo PATH=/usr/bin/hsh >> ~/.{sh0}rc'.format(sh=x[0],sh0=x[0]))
-   system('rm -rf /home/$USER/Hash/hash/')
-   print('[+] Installation Complete ...\n [+] Run "hsh" command to access...')
-   exit(0)
-else:
-     print('[-] Installation Unsuccessful ...')
-     exit()
-print('\n[+] Installation Successful, "hsh" command active ...' )
+
+def display(seq):
+    for char in seq:
+        stdout.write(char)
+        stdout.flush()
+        sleep(0.015)
+def main():
+    chdir('/home/%s'%(user())) if x[0]!=path.abspath(getcwd()) else None
+    display('\033[0;34;40m[+] \033[0;37;40mWelcome to the Hash.py installer ...\n')
+    display('\nWhat would you like to do ? [install/uninstall]: ')
+    a=input()
+    if a =='install':
+
+         install()
+    elif a =='uninstall':
+        system('sudo rm -rf /usr/bin/hsh /usr/bin/hash.py')
+        system('clear')
+        display('\033[0;31;40m[-] \033[0;37;40m Hash Generator Uninstalled ...\n')
+    else:
+        print('[-] TF did you do?? ... Exit code 1 ...')
+        exit(1)
+
+def install():
+    if path.exists('/home/%s/Hash/hash/hsh'%(user())):
+         pass
+    else:
+         system('clear')
+         display('\033[0;31;40m[-] \033[0;37;40mFatal installation files missing ...\n\033[0;31;40m[-] Please redownload files from Github @KaiViljoen/Hash/\n')
+         exit(1)
+        #Move files and assign rwx permissions
+    system('sudo cp -f /home/$USER/Hash/hash/hash.py /home/$USER/Hash/hash/hsh /usr/bin/ && sudo chown -R $USER /usr/bin/hsh /usr/bin/hash.py && sudo chmod g+rwx,u+rwx /usr/bin/hsh /usr/bin/hash.py')
+
+    #add files to PATH permanently.
+    chdir('/home/%s'%(user()))
+    if path.exists(x[0]):
+       system('echo "export PATH=/usr/bin/hash.py" >> ~/.{sh}rc && echo "export PATH=/usr/bin/hsh" >> ~/.{sh0}rc'.format(sh=x[0],sh0=x[0]))
+       system('sudo rm -rf /home/$USER/Hash/hash')
+       print('\033[0;34;40m[+] \033[0;31;40mInstallation Complete ...\n\033[0;34;40m[+] \033[0;31;40mRun "hsh" command to access...')
+       exit(0)
+    else:
+         print('\033[0;31;40m[-] Installation Unsuccessful ...')
+         exit()
+    print('\n[+] Installation Successful, "hsh" command active ...' )
+
+"""
+def repair(): # still working on this one but it's slightly more complex than I initially thought...
+    display('[+] Repair underway ...')
+    uninstall()
+    system('cd /home/$USER/ && git clone https://github.com/KaiViljoen/Hash/ && sudo chown -R $USER Hash && cd Hash')
+    system('sudo chmod u+rwx,g+rwx install.py')
+"""
+
+main()
